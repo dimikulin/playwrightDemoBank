da8a9453964bebf90b374a9e92e10b22f19299ef
DiMikulin <mikula@onet.com.pl>
2025-03-04 13:31:42 +0100

    Debug and new script test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -47,4 +47,24 @@ test.describe('Pulpit tests', () => {
                 await expect(page.locator('#show_messages')).toHaveText(`Doładowanie wykonane! ${amount},00PLN na numer ${option}`);
     });
 
+    test.only('correct balance after successful mobile top-up', async ({ page }) => {
+        // Arrange
+        const option = '500 xxx xxx';
+        const amount = '150';
+        const expectedMessage = `Doładowanie wykonane! ${amount},00PLN na numer ${option}`;
+
+        const initialBalance = await page.locator('#money_value').innerText();
+        const expectedBalance = Number(initialBalance) - Number(amount);
+
+        // Act
+        await page.locator('#widget_1_topup_receiver').selectOption(option);
+        await page.locator('#widget_1_topup_amount').fill(amount);
+        await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.getByRole('button', { name: 'doładuj telefon' }).click();
+        await page.getByTestId('close-button').click();
+
+        // Assert
+        await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
+        await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
+    });
 });
